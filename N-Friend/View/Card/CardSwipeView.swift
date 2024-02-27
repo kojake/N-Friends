1549c1bb23c6d0169c93c859b19d8ea83a629245
Yuki Takatsu <46854753+yuuki00682200@users.noreply.github.com>
2024-02-27 22:07:14 +0900

    Update CardSwipeView.swift

--- N-Friend/View/Card/CardSwipeView.swift
+++ N-Friend/View/Card/CardSwipeView.swift
@@ -23,7 +23,7 @@ struct CardSwipeView: View {
                             VStack{
                                 Image(systemName: "xmark").resizable().scaledToFit().frame(width: 35, height: 35).foregroundColor(Color.white)
                             }.frame(width: 70, height: 70).background(Color.red.opacity(0.9)).cornerRadius(50)
-                            Text("DISLIKE").font(.title).fontWeight(.bold).foregroundColor(Color.red)
+                            
                         }
                     }.padding()
                     Button(action: {
@@ -33,7 +33,7 @@ struct CardSwipeView: View {
                             VStack{
                                 Image(systemName: "heart.fill").resizable().scaledToFit().frame(width: 35, height: 35).foregroundColor(Color.white)
                             }.frame(width: 70, height: 70).background(Color.green.opacity(0.9)).cornerRadius(50)
-                            Text("LIKE").font(.title).fontWeight(.bold).foregroundColor(Color.green)
+                            
                         }
                     }
                 }.padding()
